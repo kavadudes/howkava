41f79d7627dc6cb9dd4d2c6279ea3f26df0eeafc
Ivan Villa <ivan@wearescribe.com>
2018-02-05 12:23:35 -0800

    Slider graphics added

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ Swipper init
 ************************/
 var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
-    autoplay: true
+    speed: 1500,
+    autoplay: {delay: 5000},
+    effect: 'fade'
   })
